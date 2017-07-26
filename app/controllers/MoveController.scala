f38024a04f31a9a13e3f08c336049369f84fce49
AndrewArkless <andrewarkless@blueyonder.co.uk>
2017-07-26 23:32:10 +0100

    json added

--- app/controllers/MoveController.scala
+++ app/controllers/MoveController.scala
@@ -5,12 +5,18 @@ import play.api.mvc.{Action, Controller}
 
 class MoveController extends Controller {
   def move() = Action {
-    println("HEREbv I AM!fdsdsds!!!!")
     Ok(Json.toJson("PAPER"))
   }
 
   def lastOpponentMove() = Action { request =>
-    val body=request.body.asText.getOrElse("Empty") + "ANDREW"
-    Ok(body)
+    Ok
+  }
+
+  def sayHello = Action(parse.json) { request =>
+    (request.body \ "name").asOpt[String].map { name =>
+      Ok("Hello " + name)
+    }.getOrElse {
+      BadRequest("Missing parameter [name]")
+    }
   }
 }
